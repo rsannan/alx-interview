6c1b0b872f921329cbc4aa371f41608b71d27caa
Shawn Annan <shawnrannan@gmail.com>
2023-01-27 04:24:42 +0000

    Fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Defines a Pascal's Triangle function.
+"""Defines a Pascal's Triangle function."""
 
-"""
 
 def pascal_triangle(n):
     """Represent Pascal's Triangle of size n.
@@ -11,12 +9,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    full_tri = [[1]]
-    while len(full_tri) != n:
-        tri = full_tri[-1]
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
-        full_tri.append(tmp)
-    return full_tri
+        triangles.append(tmp)
+    return triangles
