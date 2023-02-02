96d866e8021b815f5c3630e83a1544e3a7492548
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-02 17:02:45 +0000

    final fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,15 +9,26 @@ def canUnlockAll(boxes):
         True: all boxes can be opened
         False: not all boxes can be opened
     """
-    unlocked = [0]
-    for i in range(len(boxes) - 1):
-        for j in unlocked:
-            for k in boxes[j]:
-                if k not in unlocked:
-                    unlocked.append(k)
+    unlocked = []
+    keys = set()
+    i = 0
+    box_length = len(boxes)
 
-    for i in range(len(boxes)):
+    while i < box_length:
+        oldi = i
+        unlocked.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < box_length and key not in unlocked:
+                i = key
+                break
+            
+            if oldi != i:
+                continue
+            else:
+                break
+
+    for i in range(box_length):
         if i not in unlocked:
             return False
-    
     return True
