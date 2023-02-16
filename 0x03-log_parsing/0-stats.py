e26682020f437ffba25d0d37c5071b98c20f6db3
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-16 12:36:10 +0000

    fix log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,39 +3,6 @@
 
 
 import sys
-import re
-
-
-def checkline(line):
-    """Check for valid line"""
-    line_arr = line.split()
-
-# check ip address
-    ip_address = re.compile(r'\d+.\d+.\d+.\d+')
-    ip = ip_address.search(line)
-    if (ip.group() != line_arr[0]):
-        return False
-
-# check date
-    date_pattern = re.compile(r'[\d]{4}-[\d]{2}-[\d]{2}')
-    date = date_pattern.search(line)
-    if (date.group() != line_arr[2][1:]):
-        return False
-
-# check time
-    time_pattern = re.compile(r'[\d]{2}:[\d]{2}:[\d]{2}.\d+')
-    time = time_pattern.search(line)
-    if (time.group() != line_arr[3][:-1]):
-        return False
-
-# check http string
-    http_string = line_arr[4] + line_arr[5] + line_arr[6]
-    http_pattern = '"GET/projects/260HTTP/1.1"'
-    if (http_string != http_pattern):
-        return False
-
-    return True
-
 
 cache = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
@@ -45,7 +12,7 @@ counter = 0
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
-        if checkline(line):
+        if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
             if code in cache.keys():
