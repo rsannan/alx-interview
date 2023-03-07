15ebc9b98a5d0e6465d48833b9d65499e4ee597e
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-03-07 23:54:28 +0000

    add starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const rp = require('request-promise');
+
+const args = process.argv.slice(2);
+const id = args[0];
+
+function printNames (api) {
+  return new Promise(function (resolve, reject) {
+    rp(api).then((body) => {
+      const myName = JSON.parse(body);
+      resolve(myName.name);
+    });
+  });
+}
+
+function main () {
+  rp(`https://swapi-api.alx-tools.com/api/films/${id}`).then(async (body) => {
+    const myObj = JSON.parse(body);
+    for (const api of myObj.characters) {
+      const name = await printNames(api);
+      console.log(name);
+    }
+  });
+}
+
+main();
