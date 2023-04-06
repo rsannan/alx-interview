0356c3eb080596aa2558c03d53c83452b49ac532
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-04-06 23:17:29 +0000

    add prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+isWinner:
+returns winner of the game
+"""
+
+class Player:
+    """Class for players"""
+    def _init_(self, loss_count=0):
+        self.loss_count = loss_count
+    
+    def check_prime(self, num):
+        flag = False
+
+        if num == 1:
+            return False
+        elif num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    flag = True
+                    break
+
+            if flag:
+                return False
+            else:
+                return True
+
+    def check_multiples(self, array, num):
+        for number in array:
+            if number % num == 0:
+                array.remove(number)
+
+    def picknremove(self, array):
+        for number in array:
+            if self.check_prime(number):
+                self.check_multiples(array, number)
+                return 0
+        
+        return 1
+    
+    def increase_loss(self):
+        self.loss_count = self.loss_count + 1
+
+def isWinner(x, nums):
+    """finds winner of game"""
+    maria = Player()
+    ben = Player()
+
+    i = 0
+    while i < x:
+       new_arr = list(range(1,nums[i] + 1))
+       l = 0
+       while len(new_arr) >= 1:
+           l += 1
+           if (l % 2) != 0:
+               result = maria.picknremove(new_arr)
+               if result == 1:
+                   maria.increase_loss()
+                   break
+           else:
+               result = ben.picknremove(new_arr)
+               if result == 1:
+                   ben.increase_loss()
+                   break
+       i += 1
+    
+    if ben.loss_count < maria.loss_count:
+        return "Ben"
+    elif maria.loss_count < ben.loss_count:
+        return "Maria" 
+    else:
+        return None
